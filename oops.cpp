f661edcf3f803ee0766ddd18edf45c603dc2e63f
Anshul Saini <80671769+sainianshul@users.noreply.github.com>
2023-03-01 20:20:03 +0530

    Create new file

---
+++ oops.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+
+class A{
+    
+    public:
+    
+    
+    void p(){
+        
+          cout << "jai shree ram";
+        
+    }
+    
+    
+};
+
+
+int main(){
+    
+    
+    
+    A obj;
+    obj.p();
+    
+    
+    
+}
